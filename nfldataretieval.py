544878bf3890abd759df53bbd7ddf963bd0272f9
Katie <Katie>
2016-08-06 14:28:01 -0500

    Commented out a line only needed for the Linux machine to change directories

--- nfldataretieval.py
+++ nfldataretieval.py
@@ -3,8 +3,12 @@
 #Downloaded using PIP but downloaded to Anaconda3 location
 #This is only compatible with Python 2.7, so we need to move it over
 
+#The below was only needed because on the linux machine things were installed 
+#in the non default directory. Windows doesn't have this problem
+
 import sys
-sys.path.insert(0, '/home/katie/anaconda2/lib/python2.7/site-packages/')
+#sys.path.insert(0, '/home/katie/anaconda2/lib/python2.7/site-packages/')
 print '\n'.join(sys.path)
 
+#Start by importing the application
 import nflgame
