9b9304462add798116a16383b961d241f015f74d
Katie Tanner <Katiejen81@gmail.com>
2016-08-06 11:09:17 -0500

    initial commit of the misspelled data retrieval python script

---
+++ nfldataretieval.py
@@ -0,0 +1,10 @@
+#NFL Game Data Import Using Python
+#KTanner 8-6-16
+#Downloaded using PIP but downloaded to Anaconda3 location
+#This is only compatible with Python 2.7, so we need to move it over
+
+import sys
+sys.path.insert(0, '/home/katie/anaconda2/lib/python2.7/site-packages/')
+print '\n'.join(sys.path)
+
+import nflgame
