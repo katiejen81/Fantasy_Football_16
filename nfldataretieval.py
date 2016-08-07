6788e8f84ebf3af5f83123c36317b56825ee45d6
Katie <Katiejen81@gmail.com>
2016-08-06 19:25:42 -0500

    Added code that writes the schedules for the last three years to a csv file for use elsewhere

--- nfldataretieval.py
+++ nfldataretieval.py
@@ -6,9 +6,16 @@
 #The below was only needed because on the linux machine things were installed 
 #in the non default directory. Windows doesn't have this problem
 
-import sys
+#import sys
 #sys.path.insert(0, '/home/katie/anaconda2/lib/python2.7/site-packages/')
-print '\n'.join(sys.path)
+#print '\n'.join(sys.path)
+
+#find the working directory and change if needed
+import os
+os.getcwd()
+
+#I want to change the working directory - this is for Windows Machine
+os.chdir("C:\Users\Katie\Documents\Fantasy_Football_16")
 
 #Start by importing the application
 import nflgame
@@ -19,6 +26,14 @@ import csv
 
 #get the schedule for the season
 schedule_games = nflgame.sched.games
+print schedule_games
 
 #write these to csv so that I can look at them
-with open
+with open('Schedule since 2013.csv', 'wb') as csvfile:
+    schedulewriter = csv.writer(csvfile, delimiter=',')
+    schedulewriter.writerow(['Home', 'Away', 'Week', 'Year'])
+    for key in schedule_games:
+        game = schedule_games[key]
+        if game['year'] > 2012 and game['season_type'] == 'REG':
+            schedulewriter.writerow([game['home'], game['away'], game['week'], game['year']])
+            
