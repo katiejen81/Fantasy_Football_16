98163e91c46d0e9b1d163878693c672c47651561
Katie <Katiejen81@gmail.com>
2016-09-14 22:35:48 -0500

    Getting lists of players out of the API who are currently undrafted

--- Get_Fantasy_Data.py
+++ Get_Fantasy_Data.py
@@ -30,7 +30,6 @@ yql = MYQL(format='json', oauth=oauth)
 
 #First Query - this was less painful
 #Getting League information
-
 import json
 
 response = yql.select('fantasysports.leagues').where(['league_key', '=', 'nfl.l.425859'])
@@ -38,6 +37,23 @@ s = response.text
 parse = json.loads(s)
 
 #From here we will need the league key
-
 x = parse['query']['results']['league']
 league_key = x['league_key']
+
+#Run this when the access token expires
+oauth.refresh_access_token()
+oauth = OAuth1(None, None, from_file='credentials.json')
+
+
+#Now let's get a list of players within our league
+#This can't be a YQL query. Sad.
+
+url = 'http://fantasysports.yahooapis.com/fantasy/v2/league/' + league_key + '/players;status=A;sort=PTS'
+response = oauth.session.get(url, params={'format': 'json'})
+s = response.text
+parse = json.loads(s)
+
+Available_Players = parse['fantasy_content']['league'][1]['players']
+
+#need to add code here to get the next available players
+#could potentially put this in a while look to get 100 players
